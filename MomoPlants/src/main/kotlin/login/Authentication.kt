efac86a93df880f6c02fbb099b4ae375c92dc04a
Orlando Canchola <orcj17544@gmail.com>
2023-03-22 20:11:27 -0600

    Deleted Authentication class

--- MomoPlants/src/main/kotlin/login/Authentication.kt
+++
@@ -1,9 +0,0 @@
-package login
-
-class Authentication(private val username: String, private val password: String) {
-    
-    fun authenticate(username: String, password: String): Boolean {
-        return this.username == username && this.password == password
-    }
-    
-}
